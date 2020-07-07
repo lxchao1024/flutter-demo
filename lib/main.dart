a5eb777d57b3d9bb55a738e51d5b06465dba916f
lixiangchao <lixiangchao@iqiju.com>
2020-07-07 18:25:31 +0800

    :strastrawberry: 修改文件包路径、删除 main 方法无用的导包

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp1/navigator/tab_navigator.dart';
-import 'package:flutterapp1/pages/official_first_app.dart';
 import 'package:flutterapp1/pages/splash_page.dart';
 
 ///程序入口
 void main() {
-//  runApp(new MyApp());
-  ///official first app code
-  runApp(new OfficialApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatefulWidget {
